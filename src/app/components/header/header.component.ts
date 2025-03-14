fc91480200b6e2c81ad08f01a30ee98fa53db620
hashaambajwa <hashaam2002@gmail.com>
2025-03-13 23:49:29 -0400

    some logic for login page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  title = 'StoryWeavers';
+  randomQuote: string = '';
+  navLinks = ['Home', 'Stories', 'About', 'Contact'];
 
+  quotes = [
+    "Imagination is the only weapon in the war against reality.",
+    "There is no greater agony than bearing an untold story inside you.",
+    "Stories are a communal currency of humanity.",
+    "A reader lives a thousand lives before he dies.",
+    "Storytelling is the most powerful way to put ideas into the world."
+  ];
+
+  ngOnInit() {
+    this.randomQuote = this.quotes[Math.floor(Math.random() * this.quotes.length)];
+  }
 }
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  login() {
+    console.log('Logging in with:', this.username, this.password);
+    // Add authentication logic here later
+  }
+}
+
